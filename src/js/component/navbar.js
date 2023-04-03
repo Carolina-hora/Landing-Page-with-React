4f7ef371a167b115b8eefdbed822f8bd3a8c5222
Carolina-hora <carolina22hora@gmail.com>
2023-04-03 21:25:20 +0000

    Navbar completed, but hamburger icon is not grey. Had to change the background color to solved that issue

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,42 +2,57 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">
-          Navbar
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Features
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Pricing
-              </a>
-            </li>
-          </ul>
+    <div className="container-fluid">
+      <div className="row bg-dark">
+        <div className="col-sm-9 col-md-9 col-lg-9 mx-auto">
+          <nav className="navbar navbar-expand-lg bg-dark ">
+            <a className="navbar-brand text-white" href="#">
+              Start Bootstrap
+            </a>
+            <button
+              className="navbar-toggler bg-secondary"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              
+              <ul className="navbar-nav ms-auto">
+                <li className="nav-item">
+                  <a
+                    className="nav-link active text-white"
+                    aria-current="page"
+                    href="#"
+                  >
+                    Home
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-secondary" href="#">
+                    About
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-secondary" href="#">
+                    Services
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link text-secondary" href="#">
+                    Contact
+                  </a>
+                </li>
+              </ul>
+           
+            </div>
+          </nav>
         </div>
       </div>
-    </nav>
+    </div>
   );
 };

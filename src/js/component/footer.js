05737323c39ab3d260d02f0b1dce21c970924ef7
CL4UD3PT <cifpinho@gmail.com>
2023-04-03 22:17:14 +0000

    footer first commit

---
+++ src/js/component/footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Footer = () => {
+    return (
+        <div className="container-fluid bg-dark">
+            <footer className="col-sm-9 col-md-9 col-lg-9 mx-auto py-3 my-4">
+                <p className="text-center text-body-secondary">Copyright © Carol & CL4UD3PT 2023</p>
+            </footer>
+        </div>
+    );
+}

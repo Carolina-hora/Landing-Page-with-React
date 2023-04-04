91a99f38bb2127ae79793d41d6ac8ea731437e67
CL4UD3PT <cifpinho@gmail.com>
2023-04-04 17:40:46 +0000

    fix: added div container for Cards component

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -9,34 +9,43 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div><Navbar />
-		<Jumbotron />
-		<Cards buttonLabel="Find More"
-		buttonUrl="https://www.google.com/"
-		imageUrl="https://picsum.photos/id/10/500/325"
-		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-		title="My title"/>
-		<Cards buttonLabel="Find More"
-		buttonUrl="https://www.google.com/"
-		imageUrl="https://picsum.photos/id/10/500/325"
-		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-		title="My title"/>
-		<Cards buttonLabel="Find More"
-		buttonUrl="https://www.google.com/"
-		imageUrl="https://picsum.photos/id/10/500/325"
-		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-		title="My title"/>
-		<Cards buttonLabel="Find More"
-		buttonUrl="https://www.google.com/"
-		imageUrl="https://picsum.photos/id/10/500/325"
-		description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-		title="My title"/>
-		<Footer />		
-
-		</div>
-		
-	);
+  return (
+    <div>
+      <Navbar />
+      <Jumbotron />
+      <div className="col-sm-9 col-md-9 col-lg-9 mx-auto m-4 d-lg-flex">
+        <Cards
+          buttonLabel="Find More"
+          buttonUrl="https://www.google.com/"
+          imageUrl="https://picsum.photos/id/10/500/325"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="My title"
+        />
+        <Cards
+          buttonLabel="Find More"
+          buttonUrl="https://www.google.com/"
+          imageUrl="https://picsum.photos/id/10/500/325"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="My title"
+        />
+        <Cards
+          buttonLabel="Find More"
+          buttonUrl="https://www.google.com/"
+          imageUrl="https://picsum.photos/id/10/500/325"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="My title"
+        />
+        <Cards
+          buttonLabel="Find More"
+          buttonUrl="https://www.google.com/"
+          imageUrl="https://picsum.photos/id/10/500/325"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+          title="My title"
+        />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default Home;

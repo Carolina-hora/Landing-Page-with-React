8ed73e3fb799b6b0c5117e6a4ab4043b9335a842
CL4UD3PT <cifpinho@gmail.com>
2023-04-04 17:35:15 +0000

    Revert "fix: removed main div (has to be in home.jsx)"
    This reverts commit 8b1ba12baa1e01056f92e38812b209a2a2ec1984.

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -2,12 +2,15 @@ import React from "react";
 
 export const Cards = (props) => {
     return (
-        <div className="card" style={{ width: "18rem" }}>
-            <img src={props.imageUrl} className="card-img-top" alt="..." />
-            <div className="card-body text-center">
-                <h5 className="card-title">{props.title}</h5>
-                <p className="card-text">{props.description}</p>
-                <a href={props.buttonUrl} className="btn btn-primary">{props.buttonLabel}</a>
+        <div className="col-sm-9 col-md-9 col-lg-9 mx-auto m-4 d-lg-flex">
+            <div className="card" style={{ width: "18rem" }}>
+                <img src={props.imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body text-center">
+                    <h5 className="card-title">{props.title}</h5>
+                    <p className="card-text">{props.description}</p>
+                    
+                    <a href={props.buttonUrl} className="btn btn-primary">{props.buttonLabel}</a>
+                </div>
             </div>
         </div>
     );

992be64f510e3a4139b77077796eae3fbf619b94
CL4UD3PT <cifpinho@gmail.com>
2023-04-04 22:05:26 +0000

    fixes: modified some classes so there's no white space beneath footer

--- src/js/component/footer.js
+++ src/js/component/footer.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export const Footer = () => {
     return (
-        <div className="container-fluid bg-dark">
-            <footer className="col-sm-10 col-md-10 col-lg-10 mx-auto py-3 my-4">
+        <div className="container-fluid bg-dark py-5 mt-3">
+            <footer className="mx-auto my-4">
                 <p className="text-center text-white">Copyright © Carol & CL4UD3PT 2023</p>
             </footer>
         </div>

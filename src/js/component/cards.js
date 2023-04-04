4f2b6f4fabf9b1a843b39948366a5e9bfab03959
Carolina-hora <carolina22hora@gmail.com>
2023-04-04 21:02:28 +0000

    Forgot to change the images in the card to rounded-0

--- src/js/component/cards.js
+++ src/js/component/cards.js
@@ -4,7 +4,7 @@ export const Cards = (props) => {
     return (
         <div className="col-sm-12 col-md-6 col-lg-3 gx-3 gy-3">
         <div className="card">
-            <img src={props.imageUrl} className="card-img-top" alt="..." />
+            <img src={props.imageUrl} className="card-img-top rounded-0" alt="..." />
             <div className="card-body text-center">
                 <h5 className="card-title">{props.title}</h5>
                 <p className="card-text">{props.description}</p>
